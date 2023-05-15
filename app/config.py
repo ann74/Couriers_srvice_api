cb7668f5725be9331f18ea28b689b4bee54b2641
Anna Lazarenko <oleglazarenko@yandex.ru>
2023-05-16 03:16:38 +0700

    last commit

--- app/config.py
+++ app/config.py
@@ -1,2 +1,2 @@
-DB_URL = 'postgresql+asyncpg://postgres:password@localhost:5432/postgres'
-# DB_URL = 'postgresql://postgres:password@db/postgres'
+# DB_URL = 'postgresql+asyncpg://postgres:password@localhost:5432/postgres'
+DB_URL = 'postgresql+asyncpg://postgres:password@db/postgres'
